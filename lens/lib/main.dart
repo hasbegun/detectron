8bf064198118917f93ec7d0b7a245253498ec8a6
Inho Choi <inho.choi@menlosecurity.com>
2020-01-09 17:12:19 -0800

    Add Platform

--- lens/lib/main.dart
+++ lens/lib/main.dart
@@ -1,6 +1,14 @@
+import 'dart:io' show Platform;
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'home_pages.dart';
+import 'package:logger/logger.dart';
 
 void main() {
-  runApp(Home());
+  final logger = Logger();
+  logger.i('Platform $kIsWeb');
+  if (Platform.isAndroid || Platform.isIOS) {
+    runApp(Home());
+  }
 }
